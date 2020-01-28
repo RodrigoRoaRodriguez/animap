ca4cf85d3b89056e4290aa7ce4548824154ec230
RodrigoRoaRodriguez <rodrigoroarod@gmail.com>
2020-01-28 05:35:32 +0100

    fix: key prop warning

--- src/Picker.tsx
+++ src/Picker.tsx
@@ -4,15 +4,24 @@ import FormControlLabel from '@material-ui/core/FormControlLabel'
 import FormLabel from '@material-ui/core/FormLabel'
 import Radio from '@material-ui/core/Radio'
 import RadioGroup from '@material-ui/core/RadioGroup'
+interface Props {
+  values: string[]
+  onChange: (value: string) => void
+}
 
-export function Picker({ values, onChange }) {
-  const forwardValue = event => onChange(event.target.value)
+export function Picker({ values, onChange }: Props) {
+  const forwardValue = (_: unknown, value: string) => onChange(value)
   return (
     <FormControl component="fieldset">
       <FormLabel component="legend">Waveform: </FormLabel>
       <RadioGroup defaultValue={values[0]} onChange={forwardValue}>
         {values.map(value => (
-          <FormControlLabel value={value} control={<Radio />} label={value} />
+          <FormControlLabel
+            key={value}
+            value={value}
+            control={<Radio />}
+            label={value}
+          />
         ))}
       </RadioGroup>
     </FormControl>
