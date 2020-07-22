d706866b87738fd469a360b90d0aa4679e998f66
RodrigoRoaRodriguez <rodrigo.roa.rodriguez@netlight.com>
2020-07-22 14:42:00 +0200

    ♻️ extract color interpolation stuff from heatmap

--- src/Heatmap.tsx
+++ src/Heatmap.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import Texture from './Texture'
-import * as d3 from 'd3'
-import { Color } from './types/Color'
+
+
 import { useValueToColor } from './hooks/useValueToColor'
 
 interface ownProps {
@@ -13,29 +13,6 @@ interface ownProps {
 }
 
 type props = Omit<React.HTMLProps<HTMLCanvasElement>, keyof ownProps> & ownProps
-
-export function toColorInterpolator(transferFn: (value: number) => string) {
-  return function interpolator(number: number) {
-    const { r, g, b } = d3.rgb(transferFn(number))
-    return [r, g, b, 255] as Color
-  }
-}
-
-export type Args = {
-  color?: (id: any) => any
-  data?: number[][] 
-  range?: {
-    max?: number
-    min?: number
-  }
-}
-
-export const placeholderData = [
-  [255, 0, 0, 255],
-  [0, 255, 0, 255],
-  [0, 0, 255, 255],
-  [0, 0, 0, 255],
-];
 
 const Heatmap = ({ color, data, range, ...canvasProps }: props) => {
   const pixels = useValueToColor({ color, data, range })
--- src/hooks/useValueToColor.tsx
+++ src/hooks/useValueToColor.tsx
@@ -1,6 +1,30 @@
-import { normalize } from '../utils/utils';
+import * as d3 from 'd3';
+import { Color } from '../types/Color';
 import { map2d } from '../utils/array';
-import { placeholderData, Args, toColorInterpolator } from '../Heatmap';
+import { normalize } from '../utils/utils';
+
+export function toColorInterpolator(transferFn: (value: number) => string) {
+  return function interpolator(number: number) {
+    const { r, g, b } = d3.rgb(transferFn(number))
+    return [r, g, b, 255] as Color
+  }
+}
+
+export const placeholderData = [
+  [255, 0, 0, 255],
+  [0, 255, 0, 255],
+  [0, 0, 255, 255],
+  [0, 0, 0, 255],
+];
+
+export type Args = {
+  color?: (id: any) => any
+  data?: number[][] 
+  range?: {
+    max?: number
+    min?: number
+  }
+}
 
 export function useValueToColor({
   color = (id: number) => id,
