e88bdc6a4feb4e3f5ed32c45274b408f2c7a5490
RodrigoRoaRodriguez <rodrigo.roa.rodriguez@netlight.com>
2020-10-18 15:51:27 +0200

    ✨ useDux for animation
    Use my tool for state management for animation.

--- src/hooks/useAnimation.ts
+++ src/hooks/useAnimation.ts
@@ -1,4 +1,12 @@
-import { useState, useEffect } from 'react'
+import { useEffect } from 'react'
+import { useDux } from '../useDux'
+
+export const initialState = {
+  elapsed: 0,
+  start: Date.now(),
+  duration: 1000,
+  delay: 0,
+}
 
 // Hook
 export function useAnimation({
@@ -6,15 +14,15 @@ export function useAnimation({
   delay = 0,
   deps = [] as any[],
 } = {}) {
-  const [elapsed, setTime] = useState(0)
-  const [start, setStart] = useState(Date.now())
+  const { state, setState } = useDux(initialState)
+  const { start, elapsed } = state
 
-  const reset = () => setStart(Date.now())
+  const reset = () => setState({ start: Date.now() })
   // Reset when dependencies change
   useEffect(reset, deps)
 
   useEffect(
-    animationLoop(start, duration, delay, setTime),
+    animationLoop(state, setState),
     [start, duration, delay], // Re-run effect when duration or delay change
   )
 
@@ -22,22 +30,20 @@ export function useAnimation({
   const normalizedTime = Math.min(1, elapsed / duration)
 
   const setTimeTo = (percentage: number) =>
-    setStart(Date.now() - duration * percentage)
+    setState({ start: Date.now() - duration * percentage })
   // Return altered value based on our specified easing function
   return [normalizedTime, reset, setTimeTo] as const
 }
 
 function animationLoop(
-  start: number,
-  duration: number,
-  delay: number,
-  setTime: (time: number) => void,
+  { start, duration, delay, elapsed }: typeof initialState,
+  setState: React.Dispatch<Partial<typeof initialState>>,
 ) {
   let animationFrame: number
   let stopTimer: number
   // Function to be executed on each animation frame
   function onFrame() {
-    setTime(Date.now() - start)
+    setState({ elapsed: Date.now() - start })
     loop()
   }
 
@@ -49,7 +55,7 @@ function animationLoop(
     // Set a timeout to stop things when duration time elapses
     stopTimer = setTimeout(() => {
       cancelAnimationFrame(animationFrame)
-      setTime(Date.now() - start)
+      setState({ elapsed: Date.now() - start })
     }, duration)
     loop()
   }
