5e3623eb61f79e820da96b5a50580f364a53d8d2
RodrigoRoaRodriguez <rodrigo.roa.rodriguez@netlight.com>
2021-03-22 00:03:34 +0100

    ♻️  simplify animation hook

--- src/hooks/useAnimation.ts
+++ src/hooks/useAnimation.ts
@@ -4,17 +4,16 @@ import { createState, useState } from '@hookstate/core'
 
 const initialState = {}
 
-type AnimationStore = {
-  renderFrame: () => void
-} & typeof initialState
-
 export const animationState = createState({
   elapsed: 0,
   start: Date.now(),
   duration: 1000,
-  delay: 0,
   playing: true,
 })
+
+type AnimationStore = {
+  renderFrame: () => void
+} & typeof initialState
 
 const renderFrame = () =>
   animationState.elapsed.set(Date.now() - animationState.start.get())
@@ -26,26 +25,12 @@ const setTimeTo = (percentage: number) =>
 
 const reset = () => animationState.start.set(Date.now())
 
-export const play = () => animationLoop()
+export const play = () => {} // UNIMPLEMENTED
 
 // Hook
 export function useAnimation() {
   const state = useState(animationState)
 
-  useEffect(
-    animationLoop(),
-    [], // Re-run effect when duration or delay change
-  )
-
-  // Normalize, so time is on a scale from 0 to 1
-  const normalizedTime = Math.min(1, state.elapsed.get() / state.duration.get())
-
-  // Return altered value based on our specified easing function
-  return [normalizedTime, reset, setTimeTo] as const
-}
-
-function animationLoop() {
-  // }: Dux<typeof initialState, typeof acts>) {
   let animationFrame: number
   let stopTimer: ReturnType<typeof setTimeout>
   // Function to be executed on each animation frame
@@ -54,25 +39,23 @@ function animationLoop() {
     animationFrame = requestAnimationFrame(animationLoop)
   }
 
-  function onStart() {
+  useEffect(() => {
     // Set a timeout to stop things when duration time elapses
     stopTimer = setTimeout(() => {
       cancelAnimationFrame(animationFrame)
       // renderFrame()
     }, animationState.duration.get())
     animationLoop()
-  }
-
-  return () => {
-    let timerDelay: ReturnType<typeof setTimeout>
-    // Start after specified delay (defaults to 0)
-    if (animationState.playing.get())
-      timerDelay = setTimeout(onStart, animationState.delay.get())
     // Cleanup: remove listeners when the components is unmounted.
     return () => {
       clearTimeout(stopTimer)
-      clearTimeout(timerDelay)
       cancelAnimationFrame(animationFrame)
     }
-  }
+  }, [])
+
+  // Normalize, so time is on a scale from 0 to 1
+  const normalizedTime = Math.min(1, state.elapsed.get() / state.duration.get())
+
+  // Return altered value based on our specified easing function
+  return [normalizedTime, reset, setTimeTo] as const
 }
