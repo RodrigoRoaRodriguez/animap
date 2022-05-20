af233b7d0f6586a91df60ed18748814562c9360e
RodrigoRoaRodriguez <rodrigo.roa.rodriguez@netlight.com>
2022-05-20 12:27:02 +0200

    💩  firrst blog post

--- src/components/Blog/Blog.tsx
+++ src/components/Blog/Blog.tsx
@@ -1,8 +1,16 @@
+import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos'
+import { Button } from '@mui/material'
 import { lazy, Suspense } from 'react'
 const Content = lazy(() => import('./Content/Content'))
 
 export const Blog = () => (
   <>
+    <Button
+      variant="outlined"
+      sx={{ margin: '0' }}
+      href="/"
+      startIcon={<ArrowBackIosIcon />}
+    />
     <Suspense fallback={<div>Loading...</div>}>
       <Content />
     </Suspense>
